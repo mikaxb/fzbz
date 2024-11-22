2eb1f54b970b140fa0c04c5cda6c481f85d4a04e
Mikael <mikael@axblom.se>
2024-11-22 14:36:09 +0100

    Always include swagger

--- FZBZAPI/Program.cs
+++ FZBZAPI/Program.cs
@@ -13,12 +13,9 @@ builder.Services.AddFzBz();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
